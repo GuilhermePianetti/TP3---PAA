e02607ef21a24a33d40d3efcd2283ab2dff925c0
GuilhermePianetti <guipianetti@hotmail.com>
2025-01-24 22:46:47 -0300

    FIX: grafico

--- ConstrutorGrafico.py
+++ ConstrutorGrafico.py
@@ -1,29 +1,27 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 def plot_curves_from_files(file1_path, file2_path):
-    # carrrega os dados do primeiro arquivo
-    data1 = np.loadtxt(file1_path, delimiter=',')
+    # Carrega os dados, ajustando o delimitador para espaço
+    data1 = np.loadtxt(file1_path, delimiter=' ')
+    data2 = np.loadtxt(file2_path, delimiter=' ')
+
+    # Divide os dados em coordenadas x e y
     x1, y1 = data1[:, 0], data1[:, 1]
-    
-    # carrrega os dados do segundo arquivo
-    data2 = np.loadtxt(file2_path, delimiter=',')
     x2, y2 = data2[:, 0], data2[:, 1]
-    
-    # cria o gráfico
-    plt.figure(figsize=(10, 6))
-    plt.plot(x1, y1, label='Curve 1', color='blue', marker='o')
-    plt.plot(x2, y2, label='Curve 2', color='red', marker='x')
-    
-    # Adiciona título, rótulos e legenda
-    plt.title('Comparison of Two Curves')
-    plt.xlabel('X Coordinate')
-    plt.ylabel('Y Coordinate')
+
+    # Cria o gráfico
+    plt.figure(figsize=(8, 6))
+    plt.plot(x1, y1, label='Curva 1', color='blue', marker='o')
+    plt.plot(x2, y2, label='Curva 2', color='red', marker='x')
+
+    # Configurações do gráfico
+    plt.title('Curvas geradas pelos arquivos')
+    plt.xlabel('X')
+    plt.ylabel('Y')
     plt.legend()
     plt.grid(True)
-    
-    # Exibe o gráfico
     plt.show()
 
-if __name__ == "__main__":
-    plot_curves_from_files('curve1_data.txt', 'curve2_data.txt')
+# Exemplo de uso
+plot_curves_from_files('dados1.txt', 'dados2.txt')
